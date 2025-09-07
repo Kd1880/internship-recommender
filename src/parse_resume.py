2242c5c5e014db02cf269fd2d75e4a6be91f68c6
Kd1880 <kritidogra1808@gmail.com>
2025-09-07 12:40:32 +0530

    parse

--- src/parse_resume.py
+++ src/parse_resume.py
@@ -1,6 +1,6 @@
 from pyresparser import ResumeParser
 
-# Resume ka path (data folder me rakhenge)
+# Resume ka path
 data = ResumeParser('data/sample_resume.pdf').get_extracted_data()
 
 print("Parsed Resume Data:", data)
